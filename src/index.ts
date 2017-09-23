0aed06cfff00c8020acd32dd6bb763cde158ff57
Simon Janes <simon.janes@gmail.com>
2017-09-23 16:47:15 -0500

    Rand timestamp was really... bad.
    - Fix shadowed fn name.
    - Remove test users creation.
    - Redirect to home after test data created.
    - userExists utility handling either err/exist
    - actual getTimestamp
    - authentication logging into self system

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import * as _ from "lodash";
 import * as path from "path";
 import * as bcryptjs from "bcryptjs";
 import * as mongodb from "mongodb";
-import * as csurf from "csurf"; 
+import * as csurf from "csurf";
 import * as cookieParser from "cookie-parser";
-import * as bodyParser from "body-parser"; 
+import * as bodyParser from "body-parser";
 import * as session from "express-session";
 import * as morgan from "morgan";
 let flash = require('connect-flash');
@@ -22,7 +22,7 @@ let gatehouse = express();
 gatehouse.set('view engine', 'pug');
 gatehouse.set('views', './views');
 gatehouse.use(morgan('common'));
-gatehouse.use(bodyParser.urlencoded({extended: false}));
+gatehouse.use(bodyParser.urlencoded({ extended: false }));
 gatehouse.use(cookieParser());
 gatehouse.use(session({
     secret: "fidelio-alskfwenr2234oin234o32in4o23i4o3r",
@@ -47,9 +47,18 @@ interface AuthEvent {
     user_agent: string;
     result: string;
 }
+function randrange(start: number, end: number): number {
+    // This is really not intended to be precise.
+    return Math.floor(Math.random() * (end - start)) + start;
+}
 function randomTimeStamp(): string {
-    let ts = Math.floor(Math.random() * 1e8 * 1000);
-    return new Date(ts).toUTCString();
+    let year = randrange(1971,2017);
+    let month = randrange(1,12);
+    let day = randrange(1,28); // Goodness, we don't believe in anything other than February.
+    let hour = randrange(1,23);
+    let minute = randrange(1,59);
+    let sec = randrange(1,59);
+    return new Date(year, month, day, hour, minute, sec).toUTCString();
 }
 function randomIP(): string {
     // These are of course utter-nonsense.
@@ -92,8 +101,8 @@ function generateAuthEvent(): AuthEvent {
 }
 
 function recordAuthEvent(ae: AuthEvent) {
-    let log = MDB.collection('auth_events');
-    log.insertOne(ae);
+    let recorder = MDB.collection('auth_events');
+    recorder.insertOne(ae);
 }
 
 interface ExRoute {
@@ -111,13 +120,11 @@ let gatehouseRoutes: Array<ExRoute> = [
     , { method: "post", path: "/api/1/record", handler: postRecordR }
 ];
 
-function getCreateTestDataR(request, response) {
-    registerUser("admin", "swordfish");
-    registerUser("baduser", "password");
+function getCreateTestDataR(request, response) { 
     for (var i = 0; i < 1e5; i++) {
         recordAuthEvent(generateAuthEvent());
     }
-    response.end("Test data created.");
+    response.redirect("/");
 }
 
 function getFrontR(request, response): void {
@@ -135,10 +142,10 @@ function getRegisterR(request, response): void {
 }
 
 function postRegisterR(request, response): void {
-    // Unpack form parameters.
+    // Unpack form parameters. 
     let un = request.body['username'];
     let pw = request.body['password'];
-    let pwc = request.body['confirm']; 
+    let pwc = request.body['confirm'];
 
     // cases: 
     //    mismatched passwords
@@ -158,7 +165,7 @@ function postRegisterR(request, response): void {
             title: "Account Registration",
             error: "Database error returned."
         });
-        return;  
+        return;
     }
 
     function maybeExists(err, exists) {
@@ -171,11 +178,11 @@ function postRegisterR(request, response): void {
                 title: "Account Registration",
                 error: "Username already exists."
             });
-            return;    
+            return;
         }
         createUser();
     }
-    
+
     function createUser() {
         registerUser(un, pw, maybeCreated);
     }
@@ -189,9 +196,9 @@ function postRegisterR(request, response): void {
             response.render("acct/login", { title: "Account Login", message: "Account created, please log-in." });
             return;
         }
-        response.render("acct/register", { 
-            title: "Account Registration", 
-            error: "Unknown error creating account." 
+        response.render("acct/register", {
+            title: "Account Registration",
+            error: "Unknown error creating account."
         });
         return;
     }
@@ -201,12 +208,18 @@ function postRegisterR(request, response): void {
 }
 
 function userExists(un: string, cb: Function) {
-    getUser(un, (err, user) => {
+    function eitherErrorUser(err, user) {
         if (err) {
             cb(err, true); // Don't allow registration if there was an error.
+            return;
         }
-        cb(null, user.username == un);
-    });
+        if (!(err && user)) {
+            cb(null, false);
+            return;
+        }
+        cb(null, false);
+    }
+    getUser(un, eitherErrorUser);
 }
 
 function postRecordR(request, response): void {
@@ -215,19 +228,36 @@ function postRecordR(request, response): void {
 function postLoginR(request, response): void {
     let un = request.body['username'];
     let pw = request.body['password'];
-
     
+    verifyUser(request.ip, request.user_agent, un, pw, eitherErrorVerified);
+    function eitherErrorVerified(err, verified) {
+        if (err) {
+            response.render("acct/login", {
+                title: "Account Login",
+                error: "There was an error accessing the account."
+            });
+            return;
+        }
+        if (!err && !verified) {
+            response.render("acct/login", {
+                title: "Account Login",
+                error: "Access denied."
+            });
+            return;
+        }
+        response.redirect("/acct/dashboard");
+    }
 }
 
 function log_error(str: string): void {
     log(`ERROR\t ${str}`);
 }
 
- function log(str: string): void {
+function log(str: string): void {
     let ts = new Date().toString();
     process.stderr.write([ts, str].join(':') + "\n");
- }
-  
+}
+
 function registerRoute(r: ExRoute) {
     log("Route: " + [r.method, r.path].join(' '))
     switch (r.method) {
@@ -243,7 +273,7 @@ function registerRoute(r: ExRoute) {
 }
 let MDB = null;
 mongodb.MongoClient.connect(mongoDatabase, mclientHandler);
-function mclientHandler(err, db) { 
+function mclientHandler(err, db) {
     if (err !== null) {
         log_error(`Mongo connection error: ${err}`);
         // This is not production, will not reconnect in future.
@@ -268,7 +298,7 @@ function registerUser(username: string, password: string, cb?: Function) {
     let salt = bcryptjs.genSaltSync();
     let hashed = bcryptjs.hashSync(password, salt);
     let users = MDB.collection('users');
-    cb = cb ? cb : ()=>{};
+    cb = cb ? cb : () => { };
     users.insertOne({
         username: username,
         passhash: hashed,
@@ -278,20 +308,64 @@ function registerUser(username: string, password: string, cb?: Function) {
 
 function getUser(username: string, cb: Function) {
     var users = MDB.collection('users');
-    users.findOne({username: username}, cb);
+    users.findOne({ username: username }, cb);
 }
 
-function verifyUser(username: string, password: string, cb: Function) {
-    getUser(username, (err, user) => {
+function getTimestamp(): string {
+    let now = new Date();
+    return now.toUTCString();
+}
+
+function verifyUser(ip: string, user_agent: string, username: string, password: string, cb: Function) {
+    getUser(username, eitherErrorMaybeUser);
+    function eitherErrorMaybeUser(err, user) {
         if (err) {
-            cb(err, false);
+            let ae: AuthEvent = {
+                timestamp: getTimestamp(),
+                ip_address: ip,
+                username: username,
+                user_agent: user_agent,
+                result: "error"
+            };
+            recordAuthEvent(ae);
+            cb(err, null);
+            return;
+        }
+        if (!err && !user) {
+            let ae: AuthEvent = {
+                timestamp: getTimestamp(),
+                ip_address: ip,
+                username: username,
+                user_agent: user_agent,
+                result: "unknown-user"
+            };
+            recordAuthEvent(ae);            
+            cb(null, false);
+            return;
         }
         var verifier = bcryptjs.hashSync(password, user.salt);
         if (verifier === user.passhash) {
+            let ae: AuthEvent = {
+                timestamp: getTimestamp(),
+                ip_address: ip,
+                username: username,
+                user_agent: user_agent,
+                result: "auth-pass"
+            };
+            recordAuthEvent(ae);              
             cb(null, true);
+            return;
         }
+        let ae: AuthEvent = {
+            timestamp: getTimestamp(),
+            ip_address: ip,
+            username: username,
+            user_agent: user_agent,
+            result: "auth-fail"
+        };
+        recordAuthEvent(ae);         
         cb(null, false);
-    });
+    }
 }
 
 gatehouse.use(express.static('public'));
