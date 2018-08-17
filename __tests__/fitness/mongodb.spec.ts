d3ad11cd5105850b8290a976e06d4b606e5c7213
Simon Janes <simon.janes@gmail.com>
2018-08-16 22:58:03 -0500

    break out the connect/disconnect and reuse

--- __tests__/fitness/mongodb.spec.ts
+++ __tests__/fitness/mongodb.spec.ts
@@ -5,15 +5,33 @@ test(`Do we have a workable MongoDB API?`, () => {
     expect(mongodb).toBeDefined();
 });
 test(`Do we have a working test database instance?`, () => {
+    const cl = connectDatabase();
+    disconnectDatabase();
+});
+
+let theMongoClient;
+
+const connectDatabase = () => {
     const mongoDatabase = "mongodb://localhost:27017/gatehouse";
     const mongoOptions = {
         appname: "jest-mongodb.spec.ts",
     };
-    const theMongoClient = new mongodb.MongoClient(mongoDatabase, mongoOptions);
+    theMongoClient = new mongodb.MongoClient(mongoDatabase, mongoOptions);
     expect(theMongoClient).toBeDefined();
     const connectionHandler = (err, client) => {
         expect(err).toBeFalsy();
         expect(client).toBeDefined();
     };
     theMongoClient.connect(connectionHandler);
+};
+
+const disconnectDatabase = () => {
+    // I'm not happy with these psuedo-fixtures but not every test needs a setup
+    // and teardown here.
+    theMongoClient.close();
+};
+
+test(`How about doing some kind of collection work?`, () => {
+    connectDatabase();
+    disconnectDatabase();
 });
