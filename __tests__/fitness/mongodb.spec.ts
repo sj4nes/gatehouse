ba77083e89d391b0f2e1eb473b4236083781e195
Simon Janes <simon.janes@gmail.com>
2018-08-16 23:13:36 -0500

    The edge of callback hell approacheth.

--- __tests__/fitness/mongodb.spec.ts
+++ __tests__/fitness/mongodb.spec.ts
@@ -4,14 +4,15 @@ import * as mongodb from "mongodb";
 test(`Do we have a workable MongoDB API?`, () => {
     expect(mongodb).toBeDefined();
 });
-test(`Do we have a working test database instance?`, () => {
+test(`Do we have a working test database instance?`, (done) => {
     const cl = connectDatabase();
     disconnectDatabase();
+    done();
 });
 
 let theMongoClient;
 
-const connectDatabase = () => {
+const connectDatabase = (cont?) => {
     const mongoDatabase = "mongodb://localhost:27017/gatehouse";
     const mongoOptions = {
         appname: "jest-mongodb.spec.ts",
@@ -21,17 +22,28 @@ const connectDatabase = () => {
     const connectionHandler = (err, client) => {
         expect(err).toBeFalsy();
         expect(client).toBeDefined();
+        expect(client.isConnected()).toBe(true);
+        if (cont) {
+            cont();
+        }
     };
     theMongoClient.connect(connectionHandler);
 };
 
-const disconnectDatabase = () => {
+const disconnectDatabase = (cont?) => {
     // I'm not happy with these psuedo-fixtures but not every test needs a setup
     // and teardown here.
     theMongoClient.close();
+    if (cont) {
+        cont();
+    }
 };
 
-test(`How about doing some kind of collection work?`, () => {
-    connectDatabase();
-    disconnectDatabase();
+test(`How about doing some kind of database work?`, (done) => {
+    connectDatabase(() => {
+        const db = theMongoClient.db("test_mongodb");
+        expect(db).toBeDefined();
+        disconnectDatabase();
+        done();
+    });
 });
