2a4cf289da48ab1e11fcf410ff7e62d92e19ba8b
Simon Janes <simon.janes@gmail.com>
2018-08-29 15:32:43 -0500

    devise adds own tests on account model

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -4,12 +4,7 @@ class AccountTest < ActiveSupport::TestCase
   test "shouldn't be able to write an account without username" do
     badAccount = Account.new
     assert_not badAccount.save
-  end
-  test "save account with username" do
-    okAccount = Account.new
-    okAccount.username = 'admin'
-    assert okAccount.save
-  end
+  end 
   # test "the truth" do
   #   assert true
   # end
