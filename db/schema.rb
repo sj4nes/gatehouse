79ecc2a9e1b2f15396ef9eac440d289905e2838b
Simon Janes <simon.janes@gmail.com>
2018-08-29 15:13:46 -0500

    Capture migrated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180828181629) do
+ActiveRecord::Schema.define(version: 20180829201207) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "username"
     t.string "passhash"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_accounts_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
 
   create_table "auth_events", force: :cascade do |t|
