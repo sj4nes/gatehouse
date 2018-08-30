b54151013c067789fb00a5c759f42c44c0278473
Simon Janes <simon.janes@gmail.com>
2018-08-29 23:36:10 -0500

    query for single item rendered

--- app/javascript/components/AuthEventLogViewer.js
+++ app/javascript/components/AuthEventLogViewer.js
@@ -4,8 +4,10 @@ const csrfToken = document.querySelector('meta[name=csrf-token]').getAttribute('
 
 import { ApolloLink } from 'apollo-link';
 import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
 const client = new ApolloClient({
@@ -18,49 +20,76 @@ const client = new ApolloClient({
   }),
   cache: new InMemoryCache()
 });
-client
-  .query({
-    query: gql`
-      {
-        authevent(id: 1) {
-          id
-          createdAt
-          ipaddress
-        }
-      }
-    `
-  })
-  .then(result => console.log(result))
-  .catch(result => console.log(result))
+const GET_AUTHEVENT1 = gql`
+{
+  authevent(id: 1) {
+    id
+    createdAt
+    ipaddress
+  }
+}
+`;
+
+// client
+//   .query({
+//     query: gql`
+//       {
+//         authevent(id: 1) {
+//           id
+//           createdAt
+//           ipaddress
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result))
+//   .catch(result => console.log(result))
+
+function AuthEventView(props) {
+  return (<tr>
+    <td>{props.event.id}</td>
+    <td>{props.event.ipaddress}</td>
+    <td>{props.event.createdAt}</td>
+  </tr>);
+}
 class AuthEventLogViewer extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <table className="striped">
-          <thead>
-            <tr>
-              <th>Account</th>
-              <th>IP Address</th>
-              <th>Timestamp</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Test</td>
-              <td>Test</td>
-              <td>Test</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <th>Account</th>
-              <th>IP Address</th>
-              <th>Timestamp</th>
-            </tr>
+      <ApolloProvider client={client}>
+        <React.Fragment>
+          <table>
+            <thead>
+              <tr>
+                <th>Account</th>
+                <th>IP Address</th>
+                <th>Timestamp</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Query query={GET_AUTHEVENT1}>
+                {({ loading, error, data }) => {
+                  if (error) {
+                    return <tr><td>Error</td></tr>;
+                  }
+                  if (loading || !data) {
+                    return <tr><td>Fetching...</td></tr>;
+                  }
+                  return <AuthEventView event={data.authevent} />
+                  //return data.authevents.map(AuthEventView); // 
+                }}
+              </Query>
+            </tbody>
+            <tfoot>
+              <tr>
+                <th>Account</th>
+                <th>IP Address</th>
+                <th>Timestamp</th>
+              </tr>
 
-          </tfoot>
-        </table>
-      </React.Fragment>
+            </tfoot>
+          </table>
+        </React.Fragment>
+      </ApolloProvider>
     );
   }
 }
