6d903033913681e446d805ee182f08b9487fdc91
Simon Janes <simon.janes@gmail.com>
2018-08-29 23:18:55 -0500

    Pulls one event, logs it only.

--- app/javascript/components/AuthEventLogViewer.js
+++ app/javascript/components/AuthEventLogViewer.js
@@ -1,10 +1,42 @@
 import React from "react"
 import PropTypes from "prop-types"
+const csrfToken = document.querySelector('meta[name=csrf-token]').getAttribute('content');
+
+import { ApolloLink } from 'apollo-link';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'http://127.0.0.1:3000/graphql',
+    credentials: 'same-origin',
+    headers: {
+      'X-CSRF-Token': csrfToken
+    }
+  }),
+  cache: new InMemoryCache()
+});
+client
+  .query({
+    query: gql`
+      {
+        authevent(id: 1) {
+          id
+          createdAt
+          ipaddress
+        }
+      }
+    `
+  })
+  .then(result => console.log(result))
+  .catch(result => console.log(result))
 class AuthEventLogViewer extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <table class="striped">
+        <table className="striped">
           <thead>
             <tr>
               <th>Account</th>
@@ -20,9 +52,12 @@ class AuthEventLogViewer extends React.Component {
             </tr>
           </tbody>
           <tfoot>
-            <th>Account</th>
-            <th>IP Address</th>
-            <th>Timestamp</th>
+            <tr>
+              <th>Account</th>
+              <th>IP Address</th>
+              <th>Timestamp</th>
+            </tr>
+
           </tfoot>
         </table>
       </React.Fragment>
